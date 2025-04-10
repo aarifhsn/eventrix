2b2793645d62e6a5ce54320bd501bade30d23fd0
Md. Arif Hassan <aarifhsn@gmail.com>
2025-04-10 20:36:43 +0600

    updated files to php

---
+++ layouts/footer.php
@@ -0,0 +1,111 @@
+<?php include("config/config.php"); ?>
+
+<footer class="main-footer">
+    <div class="widgets-section">
+        <div class="container">
+            <div class="clearfix">
+                <div class="col-lg-12 col-sm-12 col-xs-12">
+                    <div class="row clearfix">
+
+                        <div class="footer-column col-lg-2 col-sm-6 col-xs-12">
+                            <div class="footer-widget links-widget">
+                                <h2>Links</h2>
+                                <div class="widget-content">
+                                    <ul class="list">
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="sponsors.html">Sponsors</a></li>
+                                        <li><a href="speakers.html">Speakers</a></li>
+                                        <li><a href="organizers.html">Organizers</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="footer-column col-lg-2 col-sm-6 col-xs-12">
+                            <div class="footer-widget links-widget">
+                                <h2>Pages</h2>
+                                <div class="widget-content">
+                                    <ul class="list">
+                                        <li><a href="terms.html">Terms of Use</a></li>
+                                        <li><a href="privacy.html">Privacy Policy</a></li>
+                                        <li><a href="schedule.html">Schedule</a></li>
+                                        <li><a href="contact.html">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="footer-column col-lg-4 col-sm-6 col-xs-12">
+                            <div class="footer-widget links-widget">
+                                <h2>Contact</h2>
+                                <div class="widget-content">
+                                    <ul class="list">
+                                        <li>Address: 34, Park Street, NYC, USA</li>
+                                        <li>Email: contact@example.com</li>
+                                        <li>Phone: 123-322-1248</li>
+                                    </ul>
+                                    <ul class="social-icon mt_15">
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="footer-column col-lg-4 col-sm-6 col-xs-12">
+                            <div class="footer-widget subscribe-widget">
+                                <h2>Newsletter</h2>
+                                <div class="widget-content">
+                                    <div class="newsletter-form">
+                                        <form method="post" action="">
+                                            <div class="form-group">
+                                                <input type="email" name="email" value=""
+                                                    placeholder="Enter Email Address" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="global_btn"><a class="btn_two" href="#">SUBSCRIBE</a> </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Footer Bottom-->
+    <div class="footer-bottom">
+        <div class="auto-container container">
+            <div class="text">Copyright 2024, SingleEvent. All Rights Reserved.</div>
+        </div>
+    </div>
+</footer>
+
+<script src="<?php echo BASE_URL; ?>/dist/js/jquery-3.3.1.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/popper.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/jquery.easing.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/modernizr-2.8.3.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/jquery.appear.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/jquery-countTo.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/owl.carousel.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/jquery.countdown.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/jquery.scrollTo.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/typed.js"></script>
+<script src="<?php echo BASE_URL; ?>/dist/js/custom.js"></script>
+<script>
+    $(".countDown").downCount({
+        date: '08/25/2024 12:00:00', //month/date/year   HH:MM:SS
+        offset: +6 //+GMT
+    });
+</script>
+</body>
+
+</html>
