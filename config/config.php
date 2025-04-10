386ec07c3d48c823aab5dff0d2f5432f6a2ecfb6
Md. Arif Hassan <aarifhsn@gmail.com>
2025-04-10 20:36:43 +0600

    updated files to php

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+$dbhost = 'localhost';
+$dbname = 'eventrix';
+$dbuser = 'root';
+$dbpass = '';
+try {
+    $pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $exception) {
+    echo "Connection error :" . $exception->getMessage();
+}
+
+define("BASE_URL", "http://localhost/eventrix");
+// define("ADMIN_URL", BASE_URL . "admin/");
+
+// define("SMTP_HOST", "sandbox.smtp.mailtrap.io");
+// define("SMTP_PORT", "587");
+// define("SMTP_USERNAME", "8015a4c734102a");
+// define("SMTP_PASSWORD", "a59f38709f1cc5");
+// define("SMTP_ENCRYPTION", "tls");
+// define("SMTP_FROM", "contact@yourwebsite.com");
