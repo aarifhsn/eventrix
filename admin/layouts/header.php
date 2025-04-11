c8594a6c475e120e0d7bb4ec1a445fd11d1e6462
Md. Arif Hassan <aarifhsn@gmail.com>
2025-04-11 19:52:44 +0600

    updated admin config section

--- admin/layouts/header.php
+++ admin/layouts/header.php
@@ -1,4 +1,14 @@
-<?php include("../config/config.php"); ?>
+<?php
+include("../config/config.php");
+
+ob_start();
+session_start();
+
+if (!isset($_SESSION['admin'])) {
+    header('location: ' . ADMIN_URL . '/login.php');
+}
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -51,72 +61,8 @@
     <div id="app">
         <div class="main-wrapper">
 
-            <div class="navbar-bg"></div>
-            <nav class="navbar navbar-expand-lg main-navbar">
-                <form class="form-inline mr-auto">
-                    <ul class="navbar-nav mr-3">
-                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i
-                                    class="fas fa-bars"></i></a></li>
-                    </ul>
-                </form>
-                <ul class="navbar-nav navbar-right justify-content-end rightsidetop">
-                    <li class="nav-link">
-                        <a href="" target="_blank" class="btn btn-warning">Front End</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            <img alt="image" src="uploads/user.jpg" class="rounded-circle-custom">
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="profile.html"><i class="far fa-user"></i> Edit
-                                    Profile</a></li>
-                            <li><a class="dropdown-item" href="login.html"><i class="fas fa-sign-out-alt"></i>
-                                    Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
+            <?php
 
-
-
-            <div class="main-sidebar">
-                <aside id="sidebar-wrapper">
-                    <div class="sidebar-brand">
-                        <a href="index.html">Admin Panel</a>
-                    </div>
-                    <div class="sidebar-brand sidebar-brand-sm">
-                        <a href="index.html"></a>
-                    </div>
-
-                    <ul class="sidebar-menu">
-
-                        <li class="active"><a class="nav-link" href="index.html"><i class="fas fa-hand-point-right"></i>
-                                <span>Dashboard</span></a></li>
-
-                        <li class="nav-item dropdown active">
-                            <a href="#" class="nav-link has-dropdown"><i
-                                    class="fas fa-hand-point-right"></i><span>Dropdown Items</span></a>
-                            <ul class="dropdown-menu">
-                                <li class="active"><a class="nav-link" href=""><i class="fas fa-angle-right"></i> Item
-                                        1</a></li>
-                                <li class=""><a class="nav-link" href=""><i class="fas fa-angle-right"></i> Item 2</a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li class=""><a class="nav-link" href="setting.html"><i class="fas fa-hand-point-right"></i>
-                                <span>Setting</span></a></li>
-
-                        <li class=""><a class="nav-link" href="form.html"><i class="fas fa-hand-point-right"></i>
-                                <span>Form</span></a></li>
-
-                        <li class=""><a class="nav-link" href="table.html"><i class="fas fa-hand-point-right"></i>
-                                <span>Table</span></a></li>
-
-                        <li class=""><a class="nav-link" href="invoice.html"><i class="fas fa-hand-point-right"></i>
-                                <span>Invoice</span></a></li>
-
-                    </ul>
-                </aside>
-            </div>
+            include("layouts/navbar.php");
+            include("layouts/sidebar.php");
+            ?>
