b047562241a9849f8b2145cc48d0d896780825b9
Md. Arif Hassan <aarifhsn@gmail.com>
2025-04-14 10:26:14 +0600

    fixed other files of admin section

--- admin/layouts/sidebar.php
+++ admin/layouts/sidebar.php
@@ -1,38 +1,32 @@
 <div class="main-sidebar">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Admin Panel</a>
+            <a href="<?php echo ADMIN_URL; ?>/dashboard.php">Admin Panel</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html"></a>
+            <a href="<?php echo ADMIN_URL; ?>/dashboard.php"></a>
         </div>
 
         <ul class="sidebar-menu">
-
-            <li class="active"><a class="nav-link" href="index.html"><i class="fas fa-hand-point-right"></i>
-                    <span>Dashboard</span></a></li>
-
-            <li class="nav-item dropdown active">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-hand-point-right"></i><span>Dropdown
-                        Items</span></a>
-                <ul class="dropdown-menu">
-                    <li class="active"><a class="nav-link" href=""><i class="fas fa-angle-right"></i> Item
-                            1</a></li>
-                    <li class=""><a class="nav-link" href=""><i class="fas fa-angle-right"></i> Item 2</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class=""><a class="nav-link" href="setting.html"><i class="fas fa-hand-point-right"></i>
+            <?php $current_page = basename($_SERVER['PHP_SELF']); ?>
+            <li class="<?php echo ($current_page == 'dashboard.php') ? 'active' : ''; ?>"><a class="nav-link"
+                    href="<?php echo ADMIN_URL; ?>/dashboard.php"><i class="fas fa-hand-point-right"></i>
+                    <span>Dashboard</span<?php echo ADMIN_URL; ?>/dashboard.php>
+                </a></li>
+            <li class="<?php echo ($current_page == 'setting.php') ? 'active' : ''; ?>"><a class="nav-link"
+                    href="<?php echo ADMIN_URL; ?>setting.php"><i class="fas fa-hand-point-right"></i>
                     <span>Setting</span></a></li>
 
-            <li class=""><a class="nav-link" href="form.html"><i class="fas fa-hand-point-right"></i>
+            <li class="<?php echo ($current_page == 'form.php') ? 'active' : ''; ?>"><a class="nav-link"
+                    href="<?php echo ADMIN_URL; ?>form.php"><i class="fas fa-hand-point-right"></i>
                     <span>Form</span></a></li>
 
-            <li class=""><a class="nav-link" href="table.html"><i class="fas fa-hand-point-right"></i>
+            <li class="<?php echo ($current_page == 'table.php') ? 'active' : ''; ?>"><a class="nav-link"
+                    href="<?php echo ADMIN_URL; ?>table.php"><i class="fas fa-hand-point-right"></i>
                     <span>Table</span></a></li>
 
-            <li class=""><a class="nav-link" href="invoice.html"><i class="fas fa-hand-point-right"></i>
+            <li class="<?php echo ($current_page == 'invoice.php') ? 'active' : ''; ?>"><a class="nav-link"
+                    href="<?php echo ADMIN_URL; ?>invoice.php"><i class="fas fa-hand-point-right"></i>
                     <span>Invoice</span></a></li>
 
         </ul>
