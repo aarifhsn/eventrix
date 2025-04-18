25ed5a839e8d22b9c103b519a93a665f67b273c1
Md. Arif Hassan <aarifhsn@gmail.com>
2025-04-18 23:43:46 +0600

    fixed edit and delete issues in schedule days section

--- admin/schedule-day-delete.php
+++ admin/schedule-day-delete.php
@@ -1,26 +1,21 @@
-<?php include 'layouts/top.php'; ?>
-
 <?php
+
+session_start();
+
+// Include necessary files
+include(__DIR__ . '/layouts/header.php');
+
 $statement = $pdo->prepare("SELECT * FROM schedule_days WHERE id=?");
 $statement->execute([$_REQUEST['id']]);
 $total = $statement->rowCount();
-if(!$total) {
-    header('location: '.ADMIN_URL.'schedule-day.php');
+if (!$total) {
+    header('location: ' . ADMIN_URL . 'schedule-day.php');
     exit;
 }
 
-$statement = $pdo->prepare("SELECT * FROM schedules WHERE schedule_day_id=?");
+$statement = $pdo->prepare("DELETE FROM schedule_days WHERE id=?");
 $statement->execute([$_REQUEST['id']]);
-$total = $statement->rowCount();
-if($total) {
-    $_SESSION['error_message'] = "This schedule day has some schedules. So, it can not be deleted.";
-    header('location: '.ADMIN_URL.'schedule-day.php');
-    exit;
-} else {
-    $q = $pdo->prepare("DELETE FROM schedule_days WHERE id=?");
-    $q->execute([$_REQUEST['id']]);
-    $_SESSION['success_message'] = "Data delete is successful";
-    header("location: ".ADMIN_URL."schedule-day.php");
-    exit;    
-}
+$_SESSION['success_message'] = "Data delete is successful";
+header("location: " . ADMIN_URL . "schedule-day.php");
+exit;
 
