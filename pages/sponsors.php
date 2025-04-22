6c51ea5a2113ae6a9ece65d7fe1a41285cebe593
Md. Arif Hassan <aarifhsn@gmail.com>
2025-04-22 11:47:34 +0600

    update sponsors single frontend page

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -6,63 +6,49 @@ include(__DIR__ . '/../templates/breadcrumb.php');
 // Include helpers functions
 include(__DIR__ . '/../config/helpers.php');
 
-
-// Fetch full sponsors with sponsor_categories
-$stmt = $pdo->prepare("
-  SELECT 
-    s.id AS sponsor_id,
-    s.title AS sponsor_title,
-    s.description,
-    sc.id AS sponsor_category_id,
-    sc.title AS sponsor_category_title,
-    sc.description AS sponsor_category_description
-  FROM sponsors s
-  LEFT JOIN sponsor_categories sc ON sc.id = s.sponsor_category_id
-  ORDER BY s.title ASC
-");
-$stmt->execute();
-$sponsors = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Fetch sponsor categories
+$sponsorCategories = fetchAll($pdo, 'sponsor_categories');
 
 ?>
 
 <div id="sponsorsectionList" class="pt_50 pb_50 white">
   <div class="container">
-    <?php foreach ($sponsors as $sponsor): ?>
+    <?php
+    foreach ($sponsorCategories as $category):
+      // Fetch sponsors for this category
+      $stmt = $pdo->prepare("SELECT * FROM sponsors WHERE sponsor_category_id=? ORDER BY id ASC");
+      $stmt->execute([$category['id']]);
+      $sponsors = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+      if (empty($sponsors)) {
+        continue;
+      }
+      ?>
       <div class="row">
         <div class="col-sm-1 col-lg-2"></div>
         <div class="col-xs-12 col-sm-10 col-lg-8 text-center">
           <h2 class="title-1 mb_10">
-            <span class="color_red"><?php echo $sponsor['sponsor_category_title']; ?></span>
+            <span class="color_red"><?php echo $category['title']; ?></span>
           </h2>
           <p class="heading-space">
-            <?php echo $sponsor['sponsor_category_description']; ?>
+            <?php echo $category['description']; ?>
           </p>
         </div>
         <div class="col-sm-1 col-lg-2"></div>
       </div>
 
       <div class="row pt_40 mb_50">
-        <div class="col-md-3">
-          <div class="sponsors-logo">
-            <a href="sponsor.php"><img src="dist/images/partner-1.png" class="img-responsive" alt="" /></a>
+
+        <?php
+        foreach ($sponsors as $sponsor): ?>
+          <div class="col-md-3">
+            <div class="sponsors-logo">
+              <a href="sponsor?id=<?php echo $sponsor['id']; ?>"><img
+                  src="<?php echo ADMIN_URL; ?>uploads/<?php echo $sponsor['featured_photo']; ?>" class="img-responsive"
+                  alt="" /></a>
+            </div>
           </div>
-        </div>
-        <div class="col-md-3">
-          <div class="sponsors-logo">
-            <a href="sponsor.php"><img src="dist/images/partner-2.png" class="img-responsive" alt="" /></a>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="sponsors-logo">
-            <a href="sponsor.php"><img src="dist/images/partner-3.png" class="img-responsive" alt="" /></a>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="sponsors-logo">
-            <a href="sponsor.php"><img src="dist/images/partner-4.png" class="img-responsive" alt="" />
-            </a>
-          </div>
-        </div>
+        <?php endforeach; ?>
       </div>
     <?php endforeach; ?>
   </div>
