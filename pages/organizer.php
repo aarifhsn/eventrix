53b410386df218e84a0eeab831c9e71f607f409d
Md. Arif Hassan <aarifhsn@gmail.com>
2025-04-23 11:11:09 +0600

    fixed organizers redirect single page in frontend

--- pages/organizer.php
+++ pages/organizer.php
@@ -1,7 +1,23 @@
 <?php
-
+ob_start();
 include(__DIR__ . '/../includes/header.php');
-include(__DIR__ . '/../templates/breadcrumb.php');
+
+// Validate the incoming ID
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+  header('Location: ' . BASE_URL . 'organizers');
+  exit;
+}
+
+$id = (int) $_GET['id'];
+
+$stmt = $pdo->prepare("SELECT * FROM organizers WHERE id = ?");
+$stmt->execute([$id]);
+$organizer = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$organizer) {
+  header('Location: ' . BASE_URL . 'organizers');
+  exit;
+}
 
 ?>
 
@@ -10,31 +26,15 @@ include(__DIR__ . '/../templates/breadcrumb.php');
     <div class="row">
       <div class="col-lg-4 col-sm-12 col-xs-12">
         <div class="speaker-detail-img">
-          <img src="dist/images/organizer-1.jpg" />
+          <img src="<?php echo ADMIN_URL; ?>uploads/<?php echo $organizer['photo']; ?>" />
         </div>
       </div>
       <div class="col-lg-8 col-sm-12 col-xs-12">
         <div class="speaker-detail">
-          <h2>Joseph Lynch</h2>
-          <h4 class="mb_20">Web Developer, Hugh Woods</h4>
+          <h2><?php echo htmlspecialchars($organizer['name']); ?></h2>
+          <h4 class="mb_20"><?php echo htmlspecialchars($organizer['designation']); ?></h4>
           <p>
-            Joseph Lynch is a seasoned web developer with over a decade of
-            experience in the tech industry. Specializing in front-end and
-            back-end development, Joseph has honed his skills in languages
-            such as HTML, CSS, JavaScript, and PHP, alongside a deep
-            proficiency in frameworks like Laravel and React. His portfolio
-            boasts a diverse array of projects, from sleek, responsive
-            websites for small businesses to complex web applications for
-            large corporations.
-          </p>
-          <p>
-            Beyond his technical prowess, Joseph is known for his excellent
-            communication and project management abilities. He has a knack
-            for understanding client needs and translating them into
-            functional, aesthetically pleasing digital experiences. Whether
-            working as part of a team or independently, Joseph approaches
-            each project with a meticulous attention to detail and a passion
-            for innovation.
+            <?php echo nl2br(htmlspecialchars($organizer['bio'])); ?>
           </p>
 
           <h4>More Information</h4>
@@ -42,31 +42,34 @@ include(__DIR__ . '/../templates/breadcrumb.php');
             <table class="table table-bordered">
               <tr>
                 <th><b>Address:</b></th>
-                <td>43, Park Street, NYC, USA</td>
+                <td><?php echo htmlspecialchars($organizer['address']); ?></td>
               </tr>
               <tr>
                 <th><b>Email:</b></th>
-                <td>contact@example.com</td>
+                <td><?php echo htmlspecialchars($organizer['email']); ?></td>
               </tr>
               <tr>
                 <th><b>Phone:</b></th>
-                <td>123-322-1248</td>
+                <td><?php echo htmlspecialchars($organizer['phone']); ?></td>
               </tr>
               <tr>
                 <th><b>Social:</b></th>
                 <td>
                   <ul class="social-icon">
                     <li>
-                      <a href="#"><i class="fa fa-facebook"></i></a>
+                      <a href="<?php echo htmlspecialchars($organizer['facebook']); ?>"><i
+                          class="fa fa-facebook"></i></a>
                     </li>
                     <li>
-                      <a href="#"><i class="fa fa-twitter"></i></a>
+                      <a href="<?php echo htmlspecialchars($organizer['twitter']); ?>"><i class="fa fa-twitter"></i></a>
                     </li>
                     <li>
-                      <a href="#"><i class="fa fa-linkedin"></i></a>
+                      <a href="<?php echo htmlspecialchars($organizer['linkedin']); ?>"><i
+                          class="fa fa-linkedin"></i></a>
                     </li>
                     <li>
-                      <a href="#"><i class="fa fa-instagram"></i></a>
+                      <a href="<?php echo htmlspecialchars($organizer['instagram']); ?>"><i
+                          class="fa fa-instagram"></i></a>
                     </li>
                   </ul>
                 </td>
@@ -80,3 +83,4 @@ include(__DIR__ . '/../templates/breadcrumb.php');
 </div>
 
 <?php include(__DIR__ . '/../includes/footer.php'); ?>
+<?php ob_end_flush(); ?>
