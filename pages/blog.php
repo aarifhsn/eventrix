772fb2602b48c102568be33871698d7fab6c3e7d
Md. Arif Hassan <aarifhsn@gmail.com>
2025-04-25 11:06:11 +0600

    updated frontend blog section

--- pages/blog.php
+++ pages/blog.php
@@ -4,24 +4,9 @@ ob_start();
 include(__DIR__ . '/../includes/header.php');
 include(__DIR__ . '/../templates/breadcrumb.php');
 
-
-// Validate the incoming ID
-if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
-  header('Location: ' . BASE_URL . 'blog');
-  exit;
-}
-
-$id = (int) $_GET['id'];
-
-$stmt = $pdo->prepare("SELECT * FROM posts WHERE id = ?");
-$stmt->execute([$id]);
+$stmt = $pdo->prepare("SELECT * FROM posts");
+$stmt->execute();
 $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-if (!$posts) {
-  header('Location: ' . BASE_URL . 'blog');
-  exit;
-}
-
 
 ?>
 
@@ -32,9 +17,11 @@ if (!$posts) {
         <div class="col-lg-4 col-sm-6 col-xs-12">
           <div class="blog-box text-center">
             <div class="blog-post-images">
-              <a href="<?php echo BASE_URL; ?>post?id=<?php echo $post['id']; ?>">
-                <img src="<?php echo ADMIN_URL; ?>uploads/<?php echo $post['photo']; ?>" alt="image" />
-              </a>
+              <?php if ($post['photo']): ?>
+                <a href="<?php echo BASE_URL; ?>post?id=<?php echo $post['id']; ?>">
+                  <img src="<?php echo ADMIN_URL; ?>uploads/<?php echo $post['photo']; ?>" alt="image" />
+                </a>
+              <?php endif; ?>
             </div>
             <div class="blogs-post">
               <h4>
