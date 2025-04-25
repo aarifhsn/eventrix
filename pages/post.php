6dde403e0fad4711c7fe5ce6ff7e532c013a737b
Md. Arif Hassan <aarifhsn@gmail.com>
2025-04-25 11:06:11 +0600

    updated frontend blog section

--- pages/post.php
+++ pages/post.php
@@ -1,7 +1,25 @@
 <?php
 
+ob_start();
 include(__DIR__ . '/../includes/header.php');
 include(__DIR__ . '/../templates/breadcrumb.php');
+
+// Validate the incoming ID
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+  header('Location: ' . BASE_URL . 'blog');
+  exit;
+}
+
+$id = (int) $_GET['id']; // type cast to ensure safety
+
+$stmt = $pdo->prepare("SELECT * FROM posts WHERE id = ?");
+$stmt->execute([$id]);
+$post = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$post) {
+  header('Location: ' . BASE_URL . 'blog');
+  exit;
+}
 
 ?>
 
@@ -10,40 +28,20 @@ include(__DIR__ . '/../templates/breadcrumb.php');
     <div class="row">
       <div class="page-contents col-lg-12 col-sm-12 col-xs-12">
         <div class="blogs-featured">
-          <img src="dist/images/post-1.jpg" alt="" />
+          <img src="<?php echo ADMIN_URL; ?>uploads/<?php echo $post['photo']; ?>" alt="" />
         </div>
         <div class="blog-post-meta">
           <ul class="post-meta">
-            <li class="post-date"><span>Posted On:</span> July 21, 2018</li>
+            <li class="post-date"><span>Posted On:</span> <?php echo date('F j, Y', strtotime($post['date'])); ?>
+            </li>
           </ul>
         </div>
         <div class="post-details awt-track">
+          <div class="blogs-post">
+            <h4 class="font-weight-bold mb-4"><?php echo $post['title']; ?></h4>
+          </div>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Suspendisse feugiat fringilla odio vel gravida. Phasellus nisl
-            nisl, vestibulum at blandit quis, tincidunt sed est. Nulla
-            ligula odio, imperdiet a accumsan ut, mattis nec mi. Donec
-            hendrerit magna posuere nisl viverra rhoncus. Quisque laoreet
-            urna nunc, a efficitur lorem auctor ac. Suspendisse vel pulvinar
-            orci. Nunc tincidunt quam vitae orci feugiat venenatis. Morbi in
-            ullamcorper eros. Aenean ut lorem nisi. Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit. Pellentesque ultrices feugiat
-            metus, in finibus urna. Donec vel velit pulvinar, tincidunt
-            libero facilisis, commodo nisl.
-          </p>
-          <p>
-            Vestibulum pretium elit erat, eget euismod odio commodo euismod.
-            Curabitur euismod risus turpis, vitae consectetur arcu accumsan
-            at. Donec sit amet ligula feugiat, congue lectus ut, eleifend
-            risus. Aenean odio purus, convallis eu ipsum sit amet, ornare
-            condimentum mauris. Morbi a purus et nibh sollicitudin euismod.
-            Nullam scelerisque dolor volutpat quam pharetra interdum.
-            Suspendisse arcu odio, pretium a eros sit amet, scelerisque
-            mollis odio. Ut vestibulum mauris a nulla dignissim fermentum.
-            Aliquam diam massa, elementum blandit eleifend semper, ultrices
-            vel libero. Mauris bibendum et enim in lobortis. Curabitur nec
-            consequat purus. Donec eros turpis, pulvinar vitae luctus nec,
-            venenatis sed felis.
+            <?php echo $post['content']; ?>
           </p>
         </div>
       </div>
