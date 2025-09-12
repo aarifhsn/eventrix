8ce3a24dae6e49c46bd6f5650f79ca692b4932dc
Md. Arif Hassan <aarifhsn@gmail.com>
2025-09-12 08:37:20 +0600

    updated contact pages

--- pages/contact.php
+++ pages/contact.php
@@ -1,18 +1,87 @@
 <?php
+ob_start();
 
 include(__DIR__ . '/../includes/header.php');
 include(__DIR__ . '/../templates/breadcrumb.php');
+
+// Send Email
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 
 include(__DIR__ . '/../config/helpers.php');
 
 // User Data
 
 $userData = null;
+$admin = null;
 
 if (isset($_SESSION['user']['id'])) {
   $stmt = $pdo->prepare("SELECT * FROM users WHERE id = :id");
   $stmt->execute(['id' => $_SESSION['user']['id']]);
   $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+// Get admin user(s)
+$stmt = $pdo->prepare("SELECT * FROM users WHERE role = :role");
+$stmt->execute(['role' => 'admin']);
+$admin = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['contact_form_submit'])) {
+
+  try {
+    if ($_POST['name'] == '') {
+      throw new Exception("Name can not be empty");
+    }
+    if ($_POST['email'] == '') {
+      throw new Exception("Email can not be empty");
+    }
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      throw new Exception("Please enter a valid email");
+    }
+    if ($_POST['subject'] == '') {
+      throw new Exception("Subject can not be empty");
+    }
+    if ($_POST['message'] == '') {
+      throw new Exception("Message can not be empty");
+    }
+
+    $email_message = "<h3>Visitor Information</h3>";
+    $email_message .= '<p><strong>Name:</strong><br>' . $_POST['name'] . '</p>';
+    $email_message .= '<p><strong>Email:</strong><br>' . $_POST['email'] . '</p>';
+    $email_message .= '<p><strong>Subject:</strong><br>' . $_POST['subject'] . '</p>';
+    $email_message .= '<p><strong>Message:</strong><br>' . $_POST['message'] . '</p>';
+
+    $mail = new PHPMailer(true);
+    try {
+      $mail->isSMTP();
+      $mail->Host = SMTP_HOST;
+      $mail->SMTPAuth = true;
+      $mail->Username = SMTP_USERNAME;
+      $mail->Password = SMTP_PASSWORD;
+      $mail->SMTPSecure = SMTP_ENCRYPTION;
+      $mail->Port = SMTP_PORT;
+      $mail->setFrom(SMTP_FROM);
+      $mail->addAddress($admin['email']);
+      $mail->isHTML(true);
+      $mail->Subject = 'Contact Form Message';
+      $mail->Body = $email_message;
+      $mail->send();
+    } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
+    $_SESSION['success_message'] = "Your message has been sent successfully";
+    header("location: " . BASE_URL . "contact");
+    exit;
+
+  } catch (Exception $e) {
+    $error_message = $e->getMessage();
+    $_SESSION['error_message'] = $error_message;
+    header("location: " . BASE_URL . "contact.php");
+    exit;
+  }
 }
 
 ?>
@@ -24,20 +93,20 @@ if (isset($_SESSION['user']['id'])) {
           <form class="form" method="post" action="">
             <div class="row">
               <div class="form-group col-md-6">
-                <input name="name" class="form-control" placeholder="Name" type="text" />
+                <input name="name" class="form-control" placeholder="Name *" type="text">
               </div>
               <div class="form-group col-md-6">
-                <input name="email" class="form-control" placeholder="Email" type="email" />
+                <input name="email" class="form-control" placeholder="Email *" type="email">
               </div>
               <div class="form-group col-md-12">
-                <input name="subject" class="form-control" placeholder="Subject" type="text" />
+                <input name="subject" class="form-control" placeholder="Subject *" type="text">
               </div>
               <div class="form-group col-md-12">
-                <textarea rows="3" name="message" class="form-control" placeholder="Message"></textarea>
+                <textarea rows="3" name="message" class="form-control" placeholder="Message *"></textarea>
               </div>
               <div class="col-md-12">
                 <div class="actions">
-                  <input value="Send Message" name="submit" class="btn btn-lg btn-con-bg" type="submit" />
+                  <button type="submit" class="btn btn-lg btn-con-bg" name="contact_form_submit">Send Message</button>
                 </div>
               </div>
             </div>
@@ -55,7 +124,7 @@ if (isset($_SESSION['user']['id'])) {
               </div>
               <div class="text">
                 <div class="contact-inner-text">
-                  Address: <br /><span><?php echo htmlspecialchars($userData['address']) ?? ''; ?></span>
+                  Address: <br /><span><?php echo htmlspecialchars($userData['address'] ?? 'Not Available'); ?></span>
                 </div>
               </div>
             </div>
@@ -79,7 +148,7 @@ if (isset($_SESSION['user']['id'])) {
               </div>
               <div class="text">
                 <div class="contact-inner-text">
-                  Phone: <br /><span><?php echo htmlspecialchars($userData['phone']); ?></span>
+                  Phone: <br /><span><?php echo htmlspecialchars($userData['phone'] ?? 'Not Available'); ?></span>
                 </div>
               </div>
             </div>
